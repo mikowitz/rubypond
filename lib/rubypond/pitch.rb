651c70ba15074da98002a595f89885a8af141ce6
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-20 19:22:00 -0500

    Adds documentation for pitch.

--- lib/rubypond/pitch.rb
+++ lib/rubypond/pitch.rb
@@ -12,12 +12,20 @@ module Rubypond
       end
     end
     
+    ##
+    # @private
     def to_i
       (@octave + 1) * 12 + @accidental.offset
     end
 
+    ##
+    # Returns the Lilypond representation of this <tt>Pitch</tt>, relative
+    # to either a provided <tt>relative_pitch</tt> or <tt>c4</tt> by default.
+    #
+    # @param [Pitch] relative_pitch
+    # @return [String] Lilypond
     def to_s(relative_pitch=c4)
-      natural_distance = [self, relative_pitch].sort.reverse.map{|p| p.accidental.natural_offset}.inject(&:-) % 12
+      natural_distance = [self, relative_pitch].sort.reverse.map{|pitch| pitch.accidental.natural_offset}.inject(&:-) % 12
       distance = [self.to_i, relative_pitch.to_i].sort.reverse.inject(&:-).abs
       octave_change = distance / 12
       octave_change += 1 if natural_distance >= 7 || (natural_distance.zero? && distance % 12 == 11)
@@ -28,10 +36,17 @@ module Rubypond
       end            
     end
     
+    ##
+    # @private
     def <=>(pitch)
       [self.octave, self.accidental] <=> [pitch.octave, pitch.accidental]
     end
     
+    ##
+    # Ensures that <tt>accidental</tt> is either a valid <tt>Accidental</tt> or a valid MIDI note value
+    #
+    # @private
+    # @raise [ArgumentError]
     def validate_accidental
       if octave.nil?
         raise(ArgumentError, "Invalid pitch midi value: #{accidental}") unless accidental.is_a?(Fixnum) && accidental.between?(0, 127)
@@ -40,6 +55,11 @@ module Rubypond
       end
     end
     
+    ##
+    # Ensures that <tt>octave</tt> is between 0 and 9
+    #
+    # @private
+    # @raise [ArgumentError]
     def validate_octave
       raise(ArgumentError, "Invalid pitch octave: #{octave}") if octave && (!octave.is_a?(Fixnum) || !octave.between?(0, 9))
     end
