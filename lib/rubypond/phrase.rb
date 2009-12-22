b6a4bf48385f51d274e30a9accaa1bbe159bd10a
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-22 12:57:21 -0500

    Adds documentation for phrase code.

--- lib/rubypond/phrase.rb
+++ lib/rubypond/phrase.rb
@@ -1,5 +1,6 @@
 module Rubypond
   class Phrase
+    # @private
     attr_accessor :contents
     
     def initialize &block
@@ -7,10 +8,22 @@ module Rubypond
       instance_eval(&block)
     end
     
+    ##
+    # Adds a <tt>Note</tt> with <tt>*args</tt> arguments
+    # to the <tt>Phrase</tt>'s <tt>contents</tt> array.
+    #
+    # @private
+    # @param [Pitch, Fixnum]
     def n(*args)
       @contents << Note.new(*args)
     end
     
+    ##
+    # Returns the Lilypond string representation
+    # of the <tt>Phrase</tt>'s <tt>contents</tt> array.
+    #
+    # @param [Note]
+    # @return [String] Lilypond
     def to_s(relative_note=Note.new(c4, 4))
       temp_objects, these_objects = [relative_note] + @contents, []
       temp_objects.each_with_index do |object, index|
