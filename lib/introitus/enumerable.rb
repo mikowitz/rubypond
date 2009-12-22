4a8adce058636edc5e0f08886d5c37e7579e44c1
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-22 13:42:21 -0500

    Adds the 'destructive' version of Enumerable#map_with_index.

--- lib/introitus/enumerable.rb
+++ lib/introitus/enumerable.rb
@@ -1,13 +1,23 @@
 module Enumerable
   ##
-  # Returns a new array with the results of running
-  # the given block once for each object and its
-  # position in the <tt>Enumerable</tt>.
+  # Calls block with two arguments, the <tt>object</tt> and its index, 
+  # for each <tt>object</tt> in the <tt>Enumerable</tt>.
   #
   # @param [Proc]
   # @return [Array]
   def map_with_index
     index = -1
     self.map{|element| index += 1; yield(element, index)}
+  end
+
+  ##
+  # Same as <tt>#map_with_index</tt>, but replaces <tt>self</tt>
+  # with the result.
+  #
+  # @param [Proc]
+  # @return [Array]
+  def map_with_index! &block
+    res = self.map_with_index(&block)
+    replace(res)
   end
 end
