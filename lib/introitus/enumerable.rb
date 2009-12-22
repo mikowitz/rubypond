d0ce27297d05ec9edacb1ae7b9a59fdb25e356fe
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-22 13:23:07 -0500

    Adds documentation for Enumerable#map_with_index.

--- lib/introitus/enumerable.rb
+++ lib/introitus/enumerable.rb
@@ -1,4 +1,11 @@
 module Enumerable
+  ##
+  # Returns a new array with the results of running
+  # the given block once for each object and its
+  # position in the <tt>Enumerable</tt>.
+  #
+  # @param [Proc]
+  # @return [Array]
   def map_with_index
     index = -1
     self.map{|element| index += 1; yield(element, index)}
