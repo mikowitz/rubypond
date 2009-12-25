754e3690d93770a4bcd6976d4ba10a7cb16a36cc
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-24 22:57:01 -0500

    Adds failing tests for Rubypond::INSTRUMENTS.

--- test/rubypond_test.rb
+++ test/rubypond_test.rb
@@ -6,6 +6,74 @@ describe "Rubypond" do
     Rubypond.version.should == v
   end
   
+  describe "\b::INSTRUMENTS" do
+    describe "\b' default hash" do
+      before do
+        @hash = Rubypond::INSTRUMENTS[:no_way_is_this_in_the_hash_keys]
+      end
+      
+      it "should return the correct value for :instrument_name" do
+        @hash[:instrument_name].should == "Staff"
+      end
+      it "should return the correct value for :short_instrument_name" do
+        @hash[:short_instrument_name].should == "Staff"
+      end
+      it "should return the correct value for :display_name" do
+        @hash[:display_name].should == "staff"
+      end
+      it "should return the correct value for :clef" do
+        @hash[:clef].should == "treble"
+      end
+      it "should return the correct value for :relative_c" do
+        @hash[:relative_c].should == "c4"
+      end
+    end
+
+    describe "\b[:violin]" do
+      before do
+        @hash = Rubypond::INSTRUMENTS[:violin]
+      end
+
+      it "should return the correct value for :instrument_name" do
+        @hash[:instrument_name].should == "Violin"
+      end
+      it "should return the correct value for :short_instrument_name" do
+        @hash[:short_instrument_name].should == "Vln"
+      end
+      it "should return the correct value for :display_name" do
+        @hash[:display_name].should == "violin"
+      end
+      it "should return the correct value for :clef" do
+        @hash[:clef].should == "treble"
+      end
+      it "should return the correct value for :relative_c" do
+        @hash[:relative_c].should == "c4"
+      end      
+    end
+    
+    describe "\b[:cello]" do
+      before do
+        @hash = Rubypond::INSTRUMENTS[:cello]
+      end
+
+      it "should return the correct value for :instrument_name" do
+        @hash[:instrument_name].should == "Violoncello"
+      end
+      it "should return the correct value for :short_instrument_name" do
+        @hash[:short_instrument_name].should == "Vlc."
+      end
+      it "should return the correct value for :display_name" do
+        @hash[:display_name].should == "violoncello"
+      end
+      it "should return the correct value for :clef" do
+        @hash[:clef].should == "bass"
+      end
+      it "should return the correct value for :relative_c" do
+        @hash[:relative_c].should == "c3"
+      end      
+    end
+  end
+  
   describe "\b.duration" do
     it "should return the correct value for 0.125" do
       Rubypond.duration(0.125).should == "128"
