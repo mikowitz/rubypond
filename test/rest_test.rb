41c40f56230b3f23c0806c3a887decf2b7246612
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-26 12:04:38 -0500

    Adds failing tests for rest.

---
+++ test/rest_test.rb
@@ -0,0 +1,44 @@
+require File.dirname(__FILE__) + "/test_helper"
+
+describe "Rest" do
+  before do
+    @r2 = Rest.new(2)
+    @r4 = Rest.new(4)
+  end
+  
+  it "should not have a reference_note just after creation" do
+    @r2.reference_note.should be_nil
+  end
+  
+  describe "to_s" do
+    before do
+      @ref1 = Note.new(c4, 4)
+      @ref2 = Note.new(c4, 2)
+    end
+    
+    it "should return the correct values with a reference note whose duration is 4" do
+      @r2.to_s(@ref1).should == "r8"
+      @r4.to_s(@ref1).should == "r"
+    end
+
+    it "should return the correct values with a reference note whose duration is 2" do
+      @r2.to_s(@ref1).should == "r"
+      @r4.to_s(@ref1).should == "r4"
+    end
+    
+    it "should create a value for the rest's :reference_note field" do
+      @r2.reference_note.should be_nil
+      @r2.to_s(@ref2)
+      @r2.reference_note.should == Note.new(c4, 4)
+    end
+  end
+  
+  describe "validations" do
+    [[1,2], {1 => 2}, 0, "12", 0.33333, -5, 0.013].each do |duration|
+      it "should raise an ArgumentError when created with #{duration.inspect} as its duration value" do
+        lambda { Rest.new(duration) }.should raise_error ArgumentError
+      end
+    end
+  end
+end
+
