9a561745aa9219d8d52a354f0e78cea362efdb6d
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-25 19:15:45 -0500

    Adds failing tests for score.

---
+++ test/score_test.rb
@@ -0,0 +1,129 @@
+require File.dirname(__FILE__) + "/test_helper"
+
+describe "Score" do
+  before do
+    @violin = Violin.new do
+      n c4, 4
+      n d4, 4
+      n ef4, 2
+      n g4, 4
+      n bf3, 2
+    end
+
+    @viola = Viola.new do
+      n c4, 4
+      n bf3, 4
+      n g3, 4
+      n f3, 4
+    end
+    
+    @cello = Violoncello.new do
+      n c3, 4
+      n g3, 4
+      n bf2, 4
+      n ef3, 2
+      n d3, 2
+    end
+  end
+
+  describe "with one part" do
+    before do
+      @s = Score.new do
+        part @violin
+      end
+    end
+    
+    it "should have the correct filename" do
+      @s.filename.should == "test_score.ly"
+    end
+    
+    describe "to_s" do
+      before do
+        @str = @s.to_s
+      end
+      
+      it "should have the lilypond include line" do
+        @str.should =~ /\\include \"english\.ly\"/
+      end
+      
+      it "should have the lilypond version line" do
+        @str.should =~ /\\version \"#{Rubypond.version}\"/
+      end
+      
+      it "should have a part block for each part" do
+        @str.should =~ /violin = \\relative c' \{/
+      end
+      
+      it "should have a score segment with the correct parts" do
+        @str.should =~ /score \{/
+        @str.should =~ /\\new Staff \\violin/
+      end
+    end
+  end
+
+  describe "with two parts" do
+    before do
+      @s = Score.new "My Test" do
+        part @violin
+        part @viola
+      end
+    end
+    
+    it "should have the correct filename" do
+      @s.filename.should == "my_test.ly"
+    end
+
+    describe "to_s" do
+      before do
+        @str = @s.to_s
+      end
+      
+      it "should have a part block for each part" do
+        @str.should =~ /violin = \\relative c' \{/
+        @str.should =~ /viola = \\relative c' \{/
+      end
+      
+      it "should have a score segment with the correct parts" do
+        @str.should =~ /score \{/
+        @str.should =~ /\\new Staff \\violin/
+        @str.should =~ /\\new Staff \\viola/
+      end
+      
+    end
+  end
+
+  describe "with three parts" do
+    before do
+      @s = Score.new "Full Score" do
+        part @violin
+        part @viola
+        part @cello
+      end
+    end
+    
+    it "should have the correct filename" do
+      @s.filename.should == "full_score.ly"
+    end
+    
+    describe "to_s" do
+      before do
+        @str = @s.to_s
+      end
+      
+      it "should have a part block for each part" do
+        @str.should =~ /violin = \\relative c' \{/
+        @str.should =~ /viola = \\relative c' \{/
+        @str.should =~ /violoncello = \\relative c \{/
+      end
+      
+      it "should have a score segment with the correct parts" do
+        @str.should =~ /score \{/
+        @str.should =~ /\\new Staff \\violin/
+        @str.should =~ /\\new Staff \\viola/
+        @str.should =~ /\\new Staff \\violoncello/
+      end
+    end
+  end
+end
+
+
