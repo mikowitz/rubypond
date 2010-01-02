d54101fcc08dd729f2ac3daa5482a5c448654c01
Michael Berkowitz <michael.berkowitz@gmail.com>
2010-01-01 21:53:25 -0500

    Adds basic documentation to ScoreObject.

--- lib/rubypond/score_object.rb
+++ lib/rubypond/score_object.rb
@@ -4,21 +4,31 @@ module Rubypond
   # musical objects (notes, rests, etc.) into an object's
   # <tt>@contents</tt> array.
   module ScoreObject
-    # @private
+
+    ##
+    # Add a <tt>Note</tt> to the object's contents.
+    # Params are the same as would be given to <tt>Note.new</tt>.
     def n(*args)
       @contents << Note.new(*args)
     end
 
-    # @private
+    ##
+    # Add a <tt>Rest</tt> to the object's contents.
+    # Params are the same as would be given to <tt>Note.Rest</tt>.
     def r(*args)
       @contents << Rest.new(*args)
     end
     
-    # @private
+    ##
+    # Add an <tt>InvisibleRest</tt> to the object's contents.
+    # Params are the same as would be given to <tt>InvisibleRest.new</tt>.
     def s(*args)
       @contents << InvisibleRest.new(*args)
     end
     
+    ##
+    # Add a <tt>Tuplet</tt> to the object's contents.
+    # Params are the same as would be given to <tt>Tuplet.new</tt>.
     def t(*args, &block)
       @contents << Tuplet.new(*args, &block)
     end
