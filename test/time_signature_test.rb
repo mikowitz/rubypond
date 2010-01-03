682a0bb3905d9c1615d400a8d90a6c9ab3eb577c
Michael Berkowitz <michael.berkowitz@gmail.com>
2010-01-03 01:08:06 -0500

    Fixes typos in TimeSignature tests.

--- test/time_signature_test.rb
+++ test/time_signature_test.rb
@@ -6,8 +6,8 @@ describe "TimeSignature" do
     @ts2 = TimeSignature.new(5, 16)
   end
   
-  it "should not have a :reference_duration on creation" do
-    @ts1.reference_duration.should be_nil
+  it "should not have a :reference_note on creation" do
+    @ts1.reference_note.should be_nil
   end
   
   it "should return the correct value for :to_s" do
@@ -15,9 +15,9 @@ describe "TimeSignature" do
     @ts2.to_s(Note.new(d3, 1)).should == "\\time 5/16"
   end
   
-  it "should have a value for :reference_duration after to_s is called" do
-    @ts1.reference_duration.should == Note.new(c4, 4)
-    @ts2.reference_duration.should == Note.new(d3, 1)
+  it "should have a value for :reference_note after to_s is called" do
+    @ts1.reference_note.should == Note.new(c4, 4)
+    @ts2.reference_note.should == Note.new(d3, 1)
   end
   
   describe "validations" do
