4f207c0a70ac4cac933253c48af7c4380e16e66c
Michael Berkowitz <michael.berkowitz@gmail.com>
2010-01-18 16:21:26 -0500

    Adds failing tests for VocalStaff.

--- test/staff_test.rb
+++ test/staff_test.rb
@@ -211,4 +211,46 @@ describe "Staff" do
       @string.should =~ /c g' c, g' c, g' c, g'/
     end
   end
+  
+  describe "a simple alto part" do
+    before do
+      @staff = Alto.new "II" do
+        [c4, d4, e4, fs4].each do |note|
+          n note, 4
+        end
+        lyrics "Yes, it is I."
+      end
+    end
+  
+    it "should contain the correct contents" do
+      @staff.contents.size.should == 4
+      @staff.contents.first.should == Note.new(c4, 4)
+      @staff.contents.last.should == Note.new(fs4, 4)
+    end
+
+    it "should have the correct instrument_name" do
+      @staff.instrument_name.should == "Alto II"
+    end
+  
+    it "should have the correct short_instrument_name" do
+      @staff.short_instrument_name.should == "Alt. II"
+    end
+  
+    it "should have the correct display_name" do
+      @staff.display_name.should == "altoii"
+    end
+
+    it "should have the correct score_segment" do
+      @staff.score_segment.should == "\\new Staff \\altoii"
+    end
+  
+    it "should return the correct data for to_s" do
+      @string = @staff.to_s
+      @string.should =~ /altoii \= \\relative c' \{/
+      @string.should =~ /\\set Staff\.instrumentName = \"Alto II\"/
+      @string.should =~ /\\set Staff\.shortInstrumentName = \"Alt. II\"/
+      @string.should =~ /c d e fs/
+      @string.should =~ /\\addlyrics \{ Yes, it is I. \}/
+    end
+  end
 end
